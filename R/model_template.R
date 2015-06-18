206363603f918f7702c6cd6f62701521fa7561a7
Florian Schneider <florian.schneider@univ-montp2.fr>
2015-06-18 15:08:12 +0200

    add model template (a random model)

---
+++ R/model_template.R
@@ -0,0 +1,28 @@
+# Template for model objects
+
+template <- list()
+class(template) <- "ca_model"  
+template$name <- "Random model"
+template$ref <- NA  # a bibliographic reference
+template$states <- c("1", "0")
+template$cols <- c("black", "grey50")
+# a list of default model parameters, used to validate input parameters. 
+template$parms <- list(  
+  death = 0.5,
+  birth = 0.5
+) 
+# an update function
+template$update <- function(x_old, parms) {
+  
+  x_new <- x_old
+  
+  # define update procedures depending on parms 
+  
+  rnum <- runif(length(x_old$cells))
+  x_new$cells[x_old$cells == "1" & rnum <= parms$death] <- "0"
+  x_new$cells[x_old$cells == "0" & rnum <= parms$birth] <- "1"
+  
+  return(x_new)
+}
+
+
