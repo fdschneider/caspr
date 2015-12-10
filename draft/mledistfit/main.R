70af941ab15ed9cb8ded52c4b1ac4d0b7b11934c
skefi <sonia@kefi.fr>
2015-12-11 00:51:50 +0530

    edits on main (few things)

--- draft/mledistfit/main.R
+++ draft/mledistfit/main.R
@@ -1,40 +1,46 @@
-# This code load a vector of patch size, fits several functions (MLE fitting) to the patch size distribution, compares the fits and plots the data + fitted functions.
+# This code loads a vector of patch size, fits several functions (MLE fitting) to the patch size distribution, compares the fits and plots the data + fitted functions.
 # Most of the code is based on Cosma Shalizi's code: tuvalu.santafe.edu/~aaronc/powerlaws/
-
+# Dec 2015
 
 # Load the loadfunction.Rdata file (just open the file)
 # Load package 'poweRlaw'
-
 library(poweRlaw)
 
 setwd("C:/Users/Sonia/Documents/GitHub/caspr/draft/mledistfit")
 
+# Load data
 data<- read.csv("exdata.csv", header=FALSE)
 data <- as.vector(t(data))
-pow = displ$new(data) # stores data in prep for parameter estimation
-minx = estimate_xmin(pow) #use if you need xmin to be estimated
-pow$setXmin(minx)
-est1 = estimate_pars(pow) # estimates parameters using MLE
-pow$setPars(est1$pars) # store estimated pars in the appropriate way
 
+# PL fit (analytical)
+# Note: this is for discrete data (change 'dis' to 'con' for continuous data)
+pow = displ$new(data)        # stores data in prep for parameter estimation
+minx = estimate_xmin(pow)    #use if you need xmin to be estimated
+pow$setXmin(minx)
+est1 = estimate_pars(pow)    # estimates parameters of the PL using MLE
+pow$setPars(est1$pars)       # stores estimated pars in the appropriate way
+
+# Exp fit (analytical)
+# Note: this is for discrete data (change 'dis' to 'con' for continuous data)
 exp = disexp$new(data)
 #exp$setXmin(minx)
 # For comparing distributions, keep same xmin as estimated for the powerlaw fit
-# In that case use this command in place of the above on 
+# In that case, use this command below in place of the one above: 
 exp$setXmin(pow$getXmin())
 est2 = estimate_pars(exp)
 exp$setPars(est2$pars)
 
+# Lognorm fit (analytical)
+# Note: this is for discrete data (change 'dis' to 'con' for continuous data)
 lnorm = dislnorm$new(data)
 #lnorm$setXmin(minx)
-# For comparing distributions, keep same xmin as estimated for the powerlaw fit
-# In that case use this command in place of the above on 
 lnorm$setXmin(pow$getXmin())
 est3 = estimate_pars(lnorm)
 lnorm$setPars(est3$pars)
 
-com = compare_distributions(pow,lnorm) # Compare all required combinations
-com$test_statistic # vuong test statistic (pvals calculated from this)
+# Compare fits
+com = compare_distributions(pow,exp) # Compare all required combinations (by pair)
+com$test_statistic           # vuong test statistic (pvals calculated from this)
 com$p_two_sided 
 # p = 1 implies that both distributions are equally far from the data
 # p = 0 implies that one of the distributions fits the data significantly better
@@ -45,16 +51,14 @@ com$p_one_sided
 # p-2 sided = 0; p-1 sided = 1 => 1st distribution wins
 # p-2 sided = 0; p-1 sided = 0 => 2nd distribution wins
 
-
-# 3) Compare these distributions with power law with exp cutoff (Note: cannot compare exp and powerexp yet)
-# all the attached functions have to be run
-
-exp1 = exp.fit(data,pow$getXmin()) # exponential
-pow1 = pareto.fit(data,pow$getXmin()) # power law
-lnorm1 = lnorm.fit(data,pow$getXmin()) # log-normal
+# Compare these distributions with power law with exp cutoff
+# All the distrib have to be numerically fit again for the comparison
+exp1 = exp.fit(data,pow$getXmin())        # exponential
+pow1 = pareto.fit(data,pow$getXmin())     # power law
+lnorm1 = lnorm.fit(data,pow$getXmin())    # log-normal
 powexp = powerexp.fit(data,pow$getXmin()) # power law with exponential cut-off
 
-power.powerexp.lrt(pow1,powexp) # compare pareto and powerexp
+power.powerexp.lrt(pow1,powexp)           # compare pareto and powerexp
 # gives only a single p-value. If p=1, pow is better,  If p = 0, powerexp is better
 exp.powerexp.lrt(exp1,powexp)
 # gives only a single p-value. If p=1, exp is better, If p = 0, powerexp is better
@@ -66,14 +70,11 @@ vuong(lnorm.powerexp.llr(data, lnorm1, powexp, pow$getXmin())) # compare lnorm a
 #If p.two.sided = 0, one of the distributions fits better, if p.one.sided = 1, the second distribution is a better fit.
 
 
-
-# For bootstrapping to check the fit of the best fit distribution, see the documentation for 
-# the R 'poweRlaw package
+# To evaluate how good the fit of the best distribution is: perform bootstrapping
+# (see the documentation of the R 'poweRlaw package)
 
 
 # Plot disctrete fits
-
-
   dat = plot(pow)
   po = lines(pow)
   ex = lines(exp)
@@ -106,6 +107,4 @@ vuong(lnorm.powerexp.llr(data, lnorm1, powexp, pow$getXmin())) # compare lnorm a
       ##panel.border = theme_blank(),
       ##panel.background = theme_blank()
     )
-
-
 
